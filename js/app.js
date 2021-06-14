697c9d42247d0536db3bf3a03912c706938b5add
Eston Kagwima <ekagwima745@gmail.com>
2021-06-14 15:44:45 +0300

    styling navbar

---
+++ js/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity:0.1,
+  image1:'./me.png',
+  image2:'./my-photo.png',
+  angle: Math.PI / 4,
+  displacementImage: './heightMap.png'
+
+})
